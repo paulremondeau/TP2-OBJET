2393a2f2bc8027de949dd72de50055145e204424
Paul Rémondeau <poweraider91@gmail.com>
2021-09-13 23:43:12 +0200

    C'est corrigé

--- ProjetTP/src/main/java/org/centrale/projet/objet/World.java
+++ ProjetTP/src/main/java/org/centrale/projet/objet/World.java
@@ -35,7 +35,7 @@ public class World {
         int posPeonX = generateurAleatoire.nextInt(100);
         int posPeonY = generateurAleatoire.nextInt(100); 
         Point2D posPeon = new Point2D(posPeonX,posPeonY);
-        while((Math.sqrt(Math.pow(posArcherX - posPeonX, 2) +Math.pow(posArcherY- posPeonY, 2))>5) && posArcher.equals(posPeon)){
+        while((Math.sqrt(Math.pow(posArcherX - posPeonX, 2) +Math.pow(posArcherY- posPeonY, 2))>5) || posArcher.equals(posPeon)){
             posPeonX = generateurAleatoire.nextInt(100);
             posPeonY = generateurAleatoire.nextInt(100); 
             posPeon.setX(posPeonX);
@@ -46,7 +46,7 @@ public class World {
         int posBug1X = generateurAleatoire.nextInt(100);
         int posBug1Y = generateurAleatoire.nextInt(100); 
         Point2D posBug1 = new Point2D(posBug1X,posBug1Y);
-        while((Math.sqrt(Math.pow(posBug1X - posPeonX, 2) +Math.pow(posBug1Y- posPeonY, 2))>5) && (posArcher.equals(posBug1)) && (posPeon.equals(posBug1))){
+        while((Math.sqrt(Math.pow(posBug1X - posPeonX, 2) +Math.pow(posBug1Y- posPeonY, 2))>5) || (posArcher.equals(posBug1)) || (posPeon.equals(posBug1))){
             posBug1X = generateurAleatoire.nextInt(100);
             posBug1Y = generateurAleatoire.nextInt(100); 
             posBug1.setX(posBug1X);
@@ -57,7 +57,7 @@ public class World {
         int posBug2X = generateurAleatoire.nextInt(100);
         int posBug2Y = generateurAleatoire.nextInt(100); 
         Point2D posBug2 = new Point2D(posBug2X,posBug2Y);
-        while((Math.sqrt(Math.pow(posBug2X - posPeonX, 2) +Math.pow(posBug2Y- posPeonY, 2))>5) && (posArcher.equals(posBug2)) && (posPeon.equals(posBug2)) && (posBug1.equals(posBug2))){
+        while((Math.sqrt(Math.pow(posBug2X - posPeonX, 2) +Math.pow(posBug2Y- posPeonY, 2))>5) || (posArcher.equals(posBug2)) || (posPeon.equals(posBug2)) || (posBug1.equals(posBug2))){
             posBug2X = generateurAleatoire.nextInt(100);
             posBug2Y = generateurAleatoire.nextInt(100); 
             posBug2.setX(posBug2X);
